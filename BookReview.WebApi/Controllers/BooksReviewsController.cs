65024173de968fdbd5c309f6fabc95b8fafdc1c0
Rami Hikmat <rami@ramihikmat.net>
2021-05-05 16:49:03 +1000

    Api project

---
+++ BookReview.WebApi/Controllers/BooksReviewsController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using BookReview.App.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace BookReview.WebApi.Controllers
+{
+    [ApiController]
+    [Route("Book/Reviews")]
+    public class BooksReviewsController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get([FromServices] IConfiguration configuration, [FromBody] GetBookReviewsRequest request)
+        {
+            string googleApiKey = configuration.GetSection("GoogleApiKey").Value;
+
+            var bookReviews = new List<BookReviewResult>();
+            var books = BooksAbstraction.GetBooks(googleApiKey, request.Topic);
+            foreach (var book in books)
+            {
+                bookReviews.Add(new BookReviewResult
+                {
+                    Book = book,
+                    Videos = VideosAbstraction.GetVideos(googleApiKey, book.Title)
+                });
+            }
+
+            return Ok(bookReviews);
+        }
+    }
+}
---
+++ BookReview.WebApi/Controllers/GetBookReviewsRequest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookReview.WebApi.Controllers
+{
+    public class GetBookReviewsRequest : IValidatableObject
+    {
+        [Required] public string Topic { get; set; }
+        
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            bool isMultiWord = Topic.Split().Length > 1;
+            if (isMultiWord) yield return new ValidationResult("Please only enter one word", 
+                new[] {nameof(Topic)});
+        }
+    }
+}
