c5518a7ec1bf64d6b1cbfe3930af1665f821d886
Rami Hikmat <rami@ramihikmat.net>
2021-05-05 18:46:06 +1000

    Multiple words

--- BookReview.WebApi/Controllers/GetBookReviewsRequest.cs
+++ BookReview.WebApi/Controllers/GetBookReviewsRequest.cs
@@ -1,17 +1,9 @@
-﻿using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace BookReview.WebApi.Controllers
 {
-    public class GetBookReviewsRequest : IValidatableObject
+    public class GetBookReviewsRequest
     {
         [Required] public string Topic { get; set; }
-        
-        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-        {
-            bool isMultiWord = Topic.Split().Length > 1;
-            if (isMultiWord) yield return new ValidationResult("Please only enter one word", 
-                new[] {nameof(Topic)});
-        }
     }
 }
